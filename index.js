fc5f36c959af1d74aa2252a6c65c08d7af72cb34
tania.dev <taniachristian01@gmail.com>
2025-08-29 00:25:15 +0800

    Add data

--- index.js
+++ index.js
@@ -9,49 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Training data
-const trainingsData = [
-  {
-    "id": 1,
-    "header": "Philippine Startup Challenge",
-    "subheader": "Register for hackathon",
-    "company": "DICT Region 02 - Cagayan Valley",
-    "context": "For student only.",
-    "description": [
-      "Got an idea that can change lives, solve real problems, or even become the next big business?",
-      "Gather your squad, unleash your creativity, and show the country what Cagayan Valley's best minds can do!",
-      "Whether you're in Senior High, College, or Tech-Voc, this is your chance to become a PSC Champion and help bring Region 2 back to the startup map."
-    ],
-    "registrationDeadline": "2025-09-19",
-    "submissionDeadline": "",
-    "eventDate": "",
-    "link": "https://docs.google.com/forms/d/e/1FAIpQLSejiUpsDWXAzEDsfOVZ3HyR7GWxPK7nV4IRxRqQ1ZtpxpTsmw/viewform",
-    "duration": "",
-    "category": "Hackathon",
-    "isNewlyAdded": true,
-    "capacity": 0
-  },
-  {
-    "id": 2,
-    "header": "Philippine Startup Challenge",
-    "subheader": "Register for hackathon",
-    "company": "DICT Region 11",
-    "context": "For student only.",
-    "description": [
-      "This leg is a mixture of different startups from Davao City and all provinces in the Davao Region.",
-      "The top 5 will be determined on October 2, 2025, and they will compete on October 7, 2025, face-to-face in DICT-XI Regional Office during the Semi-Final round.",
-      "The winner will advance to the Regional Pitching Competition set on October 24, 2025.",
-      "This Regional Startup Competition is your road to the Philippine Startup Challenge X National Pitching Competition!"
-    ],
-    "registrationDeadline": "2025-10-02",
-    "submissionDeadline": "",
-    "eventDate": "",
-    "link": "https://tinyurl.com/NewLegRegion11",
-    "duration": "",
-    "category": "Hackathon",
-    "isNewlyAdded": true,
-    "capacity": 0
-  }
-];
+const trainingsData = require('./trainingsData');
 
 // Routes
 app.get('/', (req, res) => {
---
+++ trainingsData.js
@@ -0,0 +1,71 @@
+const trainingsData = [
+  {
+    "id": 1,
+    "header": "Philippine Startup Challenge",
+    "subheader": "DICT's HackforGov Competition",
+    "company": "DICT Region 02 - Cagayan Valley",
+    "context": "The competition is open to all senior high school, college and university students and must be: Under 25 years of age and currently enrolled.",
+    "description": [
+      "Got an idea that can change lives, solve real problems, or even become the next big business?",
+      "Gather your squad, unleash your creativity, and show the country what Cagayan Valley's best minds can do!",
+      "Whether you're in Senior High, College, or Tech-Voc, this is your chance to become a PSC Champion and help bring Region 2 back to the startup map."
+    ],
+    "registrationDeadline": "2025-09-19",
+    "submissionDeadline": "",
+    "eventDate": "",
+    "link": "https://docs.google.com/forms/d/e/1FAIpQLSejiUpsDWXAzEDsfOVZ3HyR7GWxPK7nV4IRxRqQ1ZtpxpTsmw/viewform",
+    "duration": "",
+    "category": "Hackathon",
+    "isNewlyAdded": true,
+    "capacity": 0,
+    "createdDate": "2025-08-29"
+  },
+  {
+    "id": 2,
+    "header": "Philippine Startup Challenge",
+    "subheader": "DICT's HackforGov Competition",
+    "company": "DICT Region 11",
+    "context": "The competition is open to all senior high school, college and university students and must be: Under 25 years of age and currently enrolled.",
+    "description": [
+      "This leg is a mixture of different startups from Davao City and all provinces in the Davao Region.",
+      "The top 5 will be determined on October 2, 2025, and they will compete on October 7, 2025, face-to-face in DICT-XI Regional Office during the Semi-Final round.",
+      "The winner will advance to the Regional Pitching Competition set on October 24, 2025.",
+      "This Regional Startup Competition is your road to the Philippine Startup Challenge X National Pitching Competition!"
+    ],
+    "registrationDeadline": "2025-10-02",
+    "submissionDeadline": "",
+    "eventDate": "",
+    "link": "https://tinyurl.com/NewLegRegion11",
+    "duration": "",
+    "category": "Hackathon",
+    "isNewlyAdded": true,
+    "capacity": 0,
+    "createdDate": "2025-08-29"
+  },
+   {
+    "id": 3,
+    "header": "Philippine Startup Challenge",
+    "subheader": "DICT's HackforGov Competition",
+    "company": "DICT Region IVB - Mimaropa",
+    "context": "he competition is open to all senior high school, college and university students and must be: Under 25 years of age and currently enrolled.",
+    "description": [
+      "Attention MIMAROPA students!",
+      "Think you've got what it takes to dominate the cyber arena?",
+      "Join the 4th Edition of DICT's HackforGov Competition happening this October 9, 2025 for MIMAROPA Region!",
+      "This is your chance to represent your school on the regional stage—and who knows, maybe even take it all the way to the national and international level!",
+      "So tag your squad. Form your team. Let's show them that MIMAROPA leads with cyber skills!",
+
+    ],
+    "registrationDeadline": "2025-09-08",
+    "submissionDeadline": "",
+    "eventDate": "",
+    "link": "https://bit.ly/H4G2025-R4B-REGISTRATION",
+    "duration": "",
+    "category": "Hackathon",
+    "isNewlyAdded": true,
+    "capacity": 0,
+    "createdDate": "2025-08-29"
+  }
+];
+
+module.exports = trainingsData;
